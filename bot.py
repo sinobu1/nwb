3584dedb0618dbdb74b45c31ba866b9c0d9e7603
sinobu1 <ilykarecords@gmail.com>
2025-05-22 03:50:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telegram
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, BotCommand, ChatAction
-from telegram.constants import ParseMode  # Импорт ParseMode из telegram.constants
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, BotCommand
+from telegram.constants import ParseMode, ChatAction  # Импорт ParseMode и ChatAction из telegram.constants
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, PreCheckoutQueryHandler
 import google.generativeai as genai
 import google.api_core.exceptions
